9fc70b4182caa375818cc43a14525d68492d96d9
CORPNET\sacrlk <sachila.ranawaka@ifsworld.com>
2018-08-03 11:21:38 +0530

    initial project

--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -1,63 +1,99 @@
 import { Component, OnInit } from '@angular/core';
 import { log } from 'util';
-import { HOST_URL } from './config'
+import { HOST_URL } from './config';
+import { from } from 'rxjs';
+import { filter } from 'rxjs/operators';
+// import 'add/rxjs.opera..../filter'
 
 class EmblaImage {
-  message: string;
-  fontsize: number;
+    message: string;
+    fontsize: number;
 }
 
 class Button {
-  text: string;
-  disabled: boolean;
-  color: string;
+    text: string;
+    disabled: boolean;
+    color: string;
 }
 
 @Component({
-  selector: 'app-img-card',
-  templateUrl: './img-card.component.html',
-  styleUrls: ['./img-card.component.css']
+    selector: 'app-img-card',
+    templateUrl: './img-card.component.html',
+    styleUrls: ['./img-card.component.css']
 })
 export class ImgCardComponent implements OnInit {
 
-  private image: EmblaImage = {
-    message: 'Embla Extended Family',
-    fontsize: 40
-  };
+    private image: EmblaImage = {
+        message: 'Embla Extended Family',
+        fontsize: 40
+    };
 
-  public buttonOnline: Button = {
-    text: 'Give me another image',
-    color: 'primary',
-    disabled: false
-  };
+    public buttonOnline: Button = {
+        text: 'Give me another image',
+        color: 'primary',
+        disabled: false
+    };
 
-  public buttonOffline: Button = {
-    text: 'Sorry, you\'re offline',
-    color: 'primary',
-    disabled: true
-  };
+    public buttonOffline: Button = {
+        text: 'Sorry, you\'re offline',
+        color: 'primary',
+        disabled: true
+    };
 
-  public button: Button = this.buttonOnline;
-  public src: string;
+    public button: Button = this.buttonOnline;
+    public src: string;
+    public imageList: ImageModel[] = [];
+    private imageDataset: ImageModel[] = [];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-    var randomNo: number = Math.floor((Math.random() * 7) + 3);
-    this.src = HOST_URL + randomNo + '.jpg';
+    ngOnInit() {
+        let randomNo: number = Math.floor((Math.random() * 7) + 3);
+        this.src = HOST_URL + randomNo + '.jpg';
 
-    if (!navigator.onLine) {
-      this.button = this.buttonOffline;
+        if (!navigator.onLine) {
+            this.button = this.buttonOffline;
+        }
+        this.setImages();
     }
-  }
+    // this will create a image list with the relevant ids
+    private setImages() {
+        let imagesNumbers: number[] = [4, 7, 3];
 
-  public generateSrc(): void {
-    if (navigator.onLine) {
-      var randomNo: number = Math.floor((Math.random() * 7) + 3);
-      this.src = HOST_URL + randomNo + '.jpg';
+        imagesNumbers.forEach(element => {
+            let url = HOST_URL + element + '.jpg';
+            this.imageDataset.push({
+                url: url,
+                id: element,
+            });
+        });
+        console.log(this.imageDataset);
+        this.createObservable();
     }
-    else {
-      this.button = this.buttonOffline;
+
+    private createObservable() {
+        let result = from(this.imageDataset);
+        result
+            .pipe(
+                filter(item => item.id < 5)
+            )
+            .subscribe(item => {
+                this.imageList.push(item);
+            });
     }
-  }
+
+
+    public generateSrc(): void {
+        if (navigator.onLine) {
+            const randomNo: number = Math.floor((Math.random() * 7) + 3);
+            this.src = HOST_URL + randomNo + '.jpg';
+        } else {
+            this.button = this.buttonOffline;
+        }
+    }
+}
+
+export interface ImageModel {
+    url: string;
+    id: number;
 }
